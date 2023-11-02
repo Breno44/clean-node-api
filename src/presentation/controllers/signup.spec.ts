c36611d0a9861bc72b39ebb5df14a3f8ec553ff2
BreninCD <bhfs.2908@gmail.com>
2023-11-02 13:41:01 -0300

    feat: create first test to SignUpController

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  it('should be return status code 400 if name is not provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'email@mail.com',
+        password: '123456789',
+        passwordConfirmation: '123456789'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})

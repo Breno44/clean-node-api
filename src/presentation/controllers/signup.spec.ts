a9656459e7b5b1a060f06341b8d5fa9e4c81b22c
BreninCD <bhfs.2908@gmail.com>
2023-11-03 11:18:08 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,13 @@
 import { MissingParamName } from '../errors/missing-param-error'
 import { SignUpController } from './signup'
 
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
+
 describe('SignUp Controller', () => {
   it('should be return status code 400 if name is not provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'email@mail.com',
@@ -19,7 +23,7 @@ describe('SignUp Controller', () => {
   })
 
   it('should be return status code 400 if emil is not provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -35,7 +39,7 @@ describe('SignUp Controller', () => {
   })
 
   it('should be return status code 400 if password is not provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -51,7 +55,7 @@ describe('SignUp Controller', () => {
   })
 
   it('should be return status code 400 if passwordConfirmation is not provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',

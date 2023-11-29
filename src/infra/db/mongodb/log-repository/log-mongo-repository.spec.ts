9943e0f00a6836d22a155b6cb6c756f158a9aa6a
BreninCD <bhfs.2908@gmail.com>
2023-11-29 00:09:01 -0300

    refactor: move sut creation to a helper factory method

--- src/infra/db/mongodb/log-repository/log-mongo-repository.spec.ts
+++ src/infra/db/mongodb/log-repository/log-mongo-repository.spec.ts
@@ -2,6 +2,10 @@ import { type Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { LogMongoRepository } from './log-mongo-repository'
 
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
+
 describe('Log Mongo Repository', () => {
   let errorCollection: Collection
 
@@ -19,7 +23,7 @@ describe('Log Mongo Repository', () => {
   })
 
   it('should be add stack error on mongo', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_stack')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1)
